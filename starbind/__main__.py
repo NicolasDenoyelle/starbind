151554671f5deddc067255462b42cc00067eb6df
Nicolas Denoyelle <ndenoyelle@anl.gov>
2020-07-30 19:51:27 -0500

    Improve help

--- starbind/__main__.py
+++ starbind/__main__.py
@@ -22,22 +22,31 @@ bind the local process. Starbind will look for
 'MPI_LOCALRANKID', 'OMPI_COMM_WORLD_LOCAL_RANK' in
 environment too define the index to pick a resource
 in resource list. Only MPI processes will be bound.
+Starbind can also be used as a MPI launcher. In that
+case, MPI child processes will be tracked with ptrace
+and bound. See ptrace.
 ------------------------------------------------------
 OpenMP: starbind is used to launch an OpenMP
 application and bind its threads. Envrionment variable
 OMP_PLACES of child application will be set reflecting
-the resource list.
+the resource list. If more threads than locations are
+used, then threads are continuously packed on
+locations processing units from first location to the
+last one.
 ------------------------------------------------------
 ptrace: starbind is used to launch an application and
 bind child threads and processes. ptrace uses ptrace()
 system call to catch vfork(), fork(), clone() syscalls
 and bind child processes to the next resource in
-resource list.
+resource list. Bindings are applied in a round-robin
+order of resources and will cycle if more processes
+than available resources need to be bound.
 ------------------------------------------------------
 auto: starbind has to figure out one of above methods.
 MPI is tried first. If target environment variables
-are not set, then OpenMP is tried. OpenMP will look
-into executable linked dynamic libraries with ldd and
+are not set and no MPI library was found in the binary
+, then OpenMP is tried. OpenMP will look into
+executable linked dynamic libraries with ldd and will
 try to match a name with openmp, omp. If no match is
 found, ptrace is used.''')
 
