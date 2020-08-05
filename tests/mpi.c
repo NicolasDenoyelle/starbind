a23c930c8268f0dcd32b7775cafffa4e860f5168
Nicolas Denoyelle <ndenoyelle@anl.gov>
2020-08-05 14:29:30 -0500

    Fix status required with MPICH.

--- tests/mpi.c
+++ tests/mpi.c
@@ -7,6 +7,8 @@ void* print_cpubind(void *);
 
 int main(int argc, char **argv) {
 	int n, rank;
+	MPI_Status status;
+
 	if (MPI_Init(&argc, &argv) != MPI_SUCCESS){
 		perror("MPI_Init");
 		return -1;
@@ -20,12 +22,12 @@ int main(int argc, char **argv) {
 		rank++;
 		MPI_Send(&rank, 1, MPI_INT, rank, 0, MPI_COMM_WORLD);
 	} else if (rank+1 < n) {
-		MPI_Recv(&n, 1, MPI_INT, rank-1, 0, MPI_COMM_WORLD, NULL);
+		MPI_Recv(&n, 1, MPI_INT, rank-1, 0, MPI_COMM_WORLD, &status);
 		print_cpubind(NULL);
 		rank++;
 		MPI_Send(&rank, 1, MPI_INT, rank, 0, MPI_COMM_WORLD);
 	} else {
-		MPI_Recv(&n, 1, MPI_INT, rank-1, 0, MPI_COMM_WORLD, NULL);
+		MPI_Recv(&n, 1, MPI_INT, rank-1, 0, MPI_COMM_WORLD, &status);
 		print_cpubind(NULL);
 	}
 
